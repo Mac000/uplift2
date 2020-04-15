f249a447adc6adeb22e379b0a387c0eb164e30a2
MacTavish <mactavish10101@gmail.com>
2020-04-15 14:13:28 +0500

    Fix for broken "create RationBag" page
    .temp-bg class should have style declarations now.

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -12,12 +12,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-    <!--  Font Awesome  -->
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
     <!-- Custom Stylesheet -->
     <link rel="stylesheet" href="/css/cs_styles.css">
     <!--  Moment Js  -->
